196e509d07fd6a092af74c0eb37de393ccc022e1
Virendra Singh Solanki <72618659+theViren@users.noreply.github.com>
2022-04-28 04:59:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <title>Virendra Profile</title>
-<link rel="stylesheet" href="css/prostyle.css">
+<link rel="stylesheet" href="prostyle.css">
 </head>
 <body>
 
