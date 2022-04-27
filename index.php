96319e5f2fa7e6ec25ecd30f21d325e7a9ee1dd5
Virendra Singh Solanki <72618659+theViren@users.noreply.github.com>
2022-04-28 04:51:39 +0530

    Update and rename index.html to index.php
    I have created a portfolio website like linktree. In this, I have used HTML and CSS.

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="https://telegra.ph/file/db209ee57366824386338.jpg"/>
+<meta property="og:description" content="See My Protofolio"/>
+<link rel="icon" type="image/png" href="https://user-images.githubusercontent.com/72618659/107515187-b345d580-6bd0-11eb-8ebc-ba4cb765a36d.png"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<title>Virendra Profile</title>
+<link rel="stylesheet" href="css/prostyle.css">
+</head>
+<body>
+
+<h1 style="font-size: 1.8em; text-align: center; color:white; background-color: tomato; padding:10px;border-radius: 8em;">Welcome to My Profile</h1>
+<img id="image" src="https://telegra.ph/file/3de30fa664a2841a90df5.jpg" alt="Avatar" style="width:200px; "><br>
+<div class="card">
+  <div class="container1" style="font-size: .8em;">
+    <h3><b>Name&nbsp;<i class="fa fa-vine"></i>:</b>&nbsp;<span>Virendra Singh Solanki</span></h3> 
+    <h3><b>Email&nbsp;<i class="fa fa-paper-plane"></i>:</b>&nbsp;<span>Viren.353.vss@gmail.com</span></h3>
+    <h3><b>Address&nbsp;<i class="fa fa-address-card"></i>:</b>&nbsp;<span>Udaipur</span></h3>
+    <h3><b>D.O.B&nbsp;<i class="fa fa-birthday-cake"></i>:</b>&nbsp;<span>10 May 1999</span></h3> 
+    
+  </div>
+  
+</div><br><br>
+<div style="">
+	
+	<center><button class="info" onclick="window.open('https://bit.ly/virenaboutme'); "><i style="float: left;" class="fa fa-info-circle"></i>&nbsp;Know About Me</button></center><br>
+	<center><button class="info" onclick="window.open('https://bit.ly/virenlinkedin');"><i style="float: left;" class="fa fa-linkedin"></i>&nbsp;Check My LinkedIn</button></center><br>
+	<center><button class="info" onclick="window.open('https://bit.ly/vireninstagram');"><i style="float: left; clor: blue;" class="fa fa-instagram"></i>&nbsp;Check My Instagram</button></center><br>
+	<center><button class="info" onclick="window.open('https://bit.ly/VirenFacebook');"><i style="float: left;" class="fa fa-facebook"></i>&nbsp;Check My Facebook</button></center><br>
+	<center><button class="info" onclick="window.open('https://bit.ly/virenwhatsapp');"><i style="float: left;" class="fa fa-whatsapp"></i>&nbsp;Whatsapp Me</button></center><br>
+	<center><button class="info" onclick="window.open('https://bit.ly/VirenGmail');"><i style="float: left;" class="fa fa-envelope"></i>&nbsp;Mail Me</button></center><br>
+	<center><button class="info" onclick="window.open('https://bit.ly/VirenResumeDrive');"><i style="float: left;" class="fa fa-eye"></i>&nbsp;See My Resume</button></center><br>
+
+	
+</div>
+
+
+<a href="whatsapp://send?text=&#128071See My Resume%20&#128071%20https://theviren.herokuapp.com/" data-action="share/whatsapp/share" class="float" target="_blank"><i class="fa fa-whatsapp my-float"></i>
+</a>
+
+</body>
+</html>
